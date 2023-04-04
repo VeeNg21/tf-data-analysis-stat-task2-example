c60f4ab58111dcdd06ad8abb137718bfe5d25331
Vee21 <55953389+VeeNg21@users.noreply.github.com>
2023-04-04 15:22:36 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
-
-from scipy.stats import laplace
+from scipy.stats import t
 from scipy.stats import norm
 
 chat_id = 1374771107 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.ndarray) -> tuple:
-    alpha = 1 - p
+def solution(p: float, x: np.array) -> tuple:
     n = len(x)
-    b = np.sqrt(2*np.log(2/alpha)) / np.sqrt(n)
-    loc = np.median(x)
-    return loc - b, loc + b
+    df = n - 1
+    alpha = 1 - p
+    t_critical = t.ppf(1 - alpha / 2, df)
+    x_bar = np.mean(x)
+    s = np.std(x, ddof=1)
+    margin_of_error = t_critical * s / np.sqrt(n)
+    lower_bound = x_bar - margin_of_error
+    upper_bound = x_bar + margin_of_error
+    return (lower_bound, upper_bound)
