7dbc3378068eacadcfe33cb93a59bce3f297b475
Vee21 <55953389+VeeNg21@users.noreply.github.com>
2023-04-04 14:12:03 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,20 +1,14 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import laplace
 from scipy.stats import norm
-
 
 chat_id = 1374771107 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:
-    x_mean = x.mean()
-    x_std = x.std(ddof=1)
-    n = len(x)
+def solution(p: float, x: np.ndarray) -> tuple:
     alpha = 1 - p
-    t_val = t.ppf(1 - alpha/2, df=n-1)
-    
-    margin_error = t_val * (x_std / np.sqrt(n))
-    ci_low = x_mean - margin_error
-    ci_high = x_mean + margin_error
-    
-    return ci_low, ci_high
+    n = len(x)
+    b = np.sqrt(2*np.log(2/alpha)) / np.sqrt(n)
+    loc = np.median(x)
+    return loc - b, loc + b
